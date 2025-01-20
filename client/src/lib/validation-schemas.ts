f08d828237f492f2a3d591cac269267f97a5eb32
Petros <petros.rodinos@yahoo.com>
2025-01-20 14:56:34 +0200

    create token form

--- client/src/lib/validation-schemas.ts
+++ client/src/lib/validation-schemas.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const createTokenSchema = z.object({
   name: z.string().min(2).max(20),
   symbol: z.string().min(2).max(4),
-  totalSupply: z.number().int().positive(),
+  totalSupply: z.string(),
   imageUrl: z.string().url().optional(),
 });
--- client/src/pages/Create/index.tsx
+++ client/src/pages/Create/index.tsx
@@ -1,84 +1,86 @@
-import { FC } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { createTokenSchema } from "../../lib/validation-schemas";
 import { Button } from "../../components/ui/button";
-import { FormField, FormItem, FormLabel, FormControl, FormMessage } from "../../components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../components/ui/form";
 import { Input } from "../../components/ui/input";
+import { createTokenSchema } from "../../lib/validation-schemas";
 
-const CreateToken: FC = () => {
+export default function CreateToken() {
   const createTokenForm = useForm<z.infer<typeof createTokenSchema>>({
     resolver: zodResolver(createTokenSchema),
     defaultValues: {
       name: "",
       symbol: "",
-      totalSupply: 0,
-      imageUrl: "",
+      totalSupply: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof createTokenSchema>) {
-    console.log(values);
+  function onSubmit(data: z.infer<typeof createTokenSchema>) {
+    console.log("ASD", data);
   }
-  return (
-    <>
-      <div className="w-full mt-[60px]">
-        <div className="text-center py-8 bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-          <h1 className="text-4xl font-extrabold">Create Your Token</h1>
-          <p className="mt-2 text-lg font-medium">
-            Easily create your custom ERC20 tokens in just a few clicks.
-          </p>
-        </div>
-        <Form className="max-w-[400px] mt-5" {...createTokenForm}>
-          <form onSubmit={createTokenForm.handleSubmit(onSubmit)} className="space-y-2">
-            <FormField
-              control={createTokenForm.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem className="">
-                  <FormLabel>Token Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={createTokenForm.control}
-              name="symbol"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Token Symbol</FormLabel>
-                  <FormControl>
-                    <Input placeholder="symbol" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={createTokenForm.control}
-              name="totalSupply"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Token Supply</FormLabel>
-                  <FormControl>
-                    <Input type="number" placeholder="supply" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button className="float-left" type="submit">
-              create
-            </Button>
-          </form>
-        </Form>
-      </div>
-    </>
-  );
-};
 
-export default CreateToken;
+  return (
+    <div className="w-full mt-[60px]">
+      <div className="text-center py-8 bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+        <h1 className="text-4xl font-extrabold">Create Your Token</h1>
+        <p className="mt-2 text-lg font-medium">
+          Easily create your custom ERC20 tokens in just a few clicks.
+        </p>
+      </div>
+      <Form {...createTokenForm}>
+        <form onSubmit={createTokenForm.handleSubmit(onSubmit)} className="space-y-6 mr-2">
+          <FormField
+            control={createTokenForm.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={createTokenForm.control}
+            name="symbol"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Token Symbol</FormLabel>
+                <FormControl>
+                  <Input placeholder="symbol" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={createTokenForm.control}
+            name="totalSupply"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Token Supply</FormLabel>
+                <FormControl>
+                  <Input type="number" placeholder="supply" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="float-left" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
