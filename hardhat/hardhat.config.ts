efad051a7df74c4639d35fdd3d786358132c5e8f
Petros <petros.rodinos@yahoo.com>
2025-01-22 21:36:30 +0200

    added sepolia configuration

---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,21 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+const { vars } = require("hardhat/config");
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+};
+
+export default config;
