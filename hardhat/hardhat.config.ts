3d17d14b263cfe8944d8c4998c926caed0b38110
Petros <petros.rodinos@yahoo.com>
2025-01-23 15:30:51 +0200

    coping contracts to client

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,19 +2,22 @@ import "@nomicfoundation/hardhat-toolbox";
 import { HardhatUserConfig } from "hardhat/config";
 const { vars } = require("hardhat/config");
 
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+// const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+// const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  paths: {
+    artifacts: "../client/src/artifacts",
+  },
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
-    },
+    // sepolia: {
+    //   url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+    //   accounts: [SEPOLIA_PRIVATE_KEY],
+    // },
   },
 };
 
