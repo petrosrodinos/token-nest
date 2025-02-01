7f500a7c7438067c56ad1d9cc8b80dc4c0e647e3
Petros <petros.rodinos@yahoo.com>
2025-02-01 11:53:07 +0200

    readme

--- client/src/lib/rainbowkit.ts
+++ client/src/lib/rainbowkit.ts
@@ -1,36 +1,37 @@
 import { getDefaultConfig, darkTheme } from "@rainbow-me/rainbowkit";
-import { sepolia,polygonAmoy } from "wagmi/chains";
-import { Chain } from "@rainbow-me/rainbowkit";
+import { sepolia } from "wagmi/chains";
+// import {polygonAmoy } from "wagmi/chains";
+// import { Chain } from "@rainbow-me/rainbowkit";
 import { RAINBOW_PROJECT_ID } from "./constants";
 
-const hardhatLocal = {
-  id: 31337,
-  name: "localhost",
-  iconUrl: "https://hardhat.org/favicon.ico",
-  iconBackground: "#fff",
-  nativeCurrency: {
-    name: "Ethereum",
-    symbol: "ETH",
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: { http: ["http://127.0.0.1:8545"] },
-  },
-  blockExplorers: {
-    default: { name: "Hardhat", url: "http://localhost:8545" },
-  },
-  contracts: {
-    multicall3: {
-      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-      blockCreated: 0,
-    },
-  },
-} as const satisfies Chain;
+// const hardhatLocal = {
+//   id: 31337,
+//   name: "localhost",
+//   iconUrl: "https://hardhat.org/favicon.ico",
+//   iconBackground: "#fff",
+//   nativeCurrency: {
+//     name: "Ethereum",
+//     symbol: "ETH",
+//     decimals: 18,
+//   },
+//   rpcUrls: {
+//     default: { http: ["http://127.0.0.1:8545"] },
+//   },
+//   blockExplorers: {
+//     default: { name: "Hardhat", url: "http://localhost:8545" },
+//   },
+//   contracts: {
+//     multicall3: {
+//       address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+//       blockCreated: 0,
+//     },
+//   },
+// } as const satisfies Chain;
 
 export const wagmiConfig = getDefaultConfig({
   appName: "wagmi-tokennest",
   projectId: RAINBOW_PROJECT_ID,
-  chains: [sepolia, hardhatLocal,polygonAmoy],
+  chains: [sepolia],
 });
 
 export const wagmiTheme = darkTheme({
